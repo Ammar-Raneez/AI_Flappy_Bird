145b7cef3deb283c012ab70cd30cc289763638e7
Ammar <ammarraneez@gmail.com>
2021-01-03 19:38:21 +0530

    base class done

--- flappy_bird.py
+++ flappy_bird.py
@@ -103,17 +103,17 @@ class Pipe:
         self.x -= self.VEL
 
     def draw(self, win):
-        win.blit(self.PIPE_TOP, (self.x, self.y))
-        win.blit(self.PIPE_BOTTOM, (self, x, self.y))
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
     
-    #collisions
+    #collisionsW
     def collide(self, bird):
         bird_mask = bird.get_mask()
         top_mask = pygame.mask.from_surface(self.PIPE_TOP)
         bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
 
-        top_offset = (self.x - bird.x, self.top - round(bird, y))
-        bottom_offset = (self.x - bird.x, self.bottom - round(bird, y))
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
         #collision between bird and bottom pipe
         bottom_point = bird_mask.overlap(bottom_mask, bottom_offset)
         top_point = bird_mask.overlap(top_mask, top_offset)
@@ -122,7 +122,28 @@ class Pipe:
         return top_point or bottom_point
 
 class Base:
+    VEL = 5
+    WIDTH = BASE_IMAGE.get_width()
+    IMG = BASE_IMAGE
 
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+    
+    #creating two images, and move both at the same time towards the left
+    #so the second one replaces the first, the first cycles to the front n so does the second
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 += self.WIDTH
+        if self.x2 + self.WIDTH < 0:
+            self.x2 += self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
 
 def draw_window(win, bird):
